28fee2e139ebce5f2b879bf75f25d2aa8aeed954
Janez Rotman <janez@janez.at>
2026-09-13 22:54:20 +0200

    add model and interfaces

---
+++ src/Ausbeute.Core/Model/Json.cs
@@ -0,0 +1,39 @@
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+using System.Text.Json.Serialization;
+
+namespace Ausbeute.Model;
+
+[JsonSourceGenerationOptions(
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    UseStringEnumConverter = true,
+    WriteIndented = true)]
+[JsonSerializable(typeof(Case))]
+[JsonSerializable(typeof(RuleSet))]
+[JsonSerializable(typeof(RulesCache))]
+[JsonSerializable(typeof(Change))]
+[JsonSerializable(typeof(List<Change>))]
+[JsonSerializable(typeof(Ingredient))]
+[JsonSerializable(typeof(ArticleMapping))]
+[JsonSerializable(typeof(Product))]
+[JsonSerializable(typeof(YieldRule))]
+[JsonSerializable(typeof(Invoice))]
+[JsonSerializable(typeof(OcrPage))]
+[JsonSerializable(typeof(List<OcrPage>))]
+public sealed partial class ModelJsonContext : JsonSerializerContext
+{
+}
+
+public static class Json
+{
+    static readonly JsonSerializerOptions Options = new(ModelJsonContext.Default.Options) { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
+
+    public static string Serialize<T>(T value) =>
+        JsonSerializer.Serialize(value, (JsonTypeInfo<T>)Options.GetTypeInfo(typeof(T)));
+
+    public static T Deserialize<T>(ReadOnlySpan<byte> data) =>
+        JsonSerializer.Deserialize(data, (JsonTypeInfo<T>)ModelJsonContext.Default.GetTypeInfo(typeof(T))!)
+        ?? throw new JsonException("leeres Dokument");
+}
