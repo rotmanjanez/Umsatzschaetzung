f254550433e0f28a1884bc93866f5c70a408c350
Janez Rotman <janez@janez.at>
2026-09-16 23:10:39 +0200

    add platform rasterizer to macos

--- src/Umsatzschätzung.App/App.axaml.cs
+++ src/Umsatzschätzung.App/App.axaml.cs
@@ -82,6 +82,8 @@ public partial class App : Application
         IPdfPages? pdf = null;
 #if WINDOWS
         if (OperatingSystem.IsWindows()) pdf = new WindowsPdfPages();
+#else
+        if (OperatingSystem.IsMacOS()) pdf = new MacPdfPages();
 #endif
         var printer = new WebViewPdfPrinter();
         owned.Add(printer);
